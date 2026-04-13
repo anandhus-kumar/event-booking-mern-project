0382dc94dc258475acaaef2dabc1a9e660f5bb2d
anandhus-kumar <anandhusk22@gmail.com>
2026-04-13 22:09:56 +0530

    Initialize project

---
+++ backend/controller/authController.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+
+export async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+}
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+//   mongodb connect
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to Mongo", error);
+  });
+
+app.listen(PORT, () => console.log("server is running"));
