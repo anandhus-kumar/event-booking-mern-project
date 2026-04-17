ed4cd0b74d735333c0e76f2417111f1175114d63
anandhus-kumar <anandhusk22@gmail.com>
2026-04-17 21:19:13 +0530

    feat:axios and context config

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,59 @@
+
+
+import { useEffect, useState } from "react";
+import { AuthContext } from "./authContext";
+import api from "../utils/axios";
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = async (userData) => {
+    try {
+      const { data } = await api.post("/auth/login", userData);
+      setUser(data);
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", data.token);
+      return data;
+    } catch (error) {
+      console.error("Login failed", error);
+      throw error;
+    }
+  };
+
+  const verifyOtp = async (userData) => {
+    try {
+      const { data } = await api.post("/auth/verify-otp", userData);
+      setUser(data);
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", data.token);
+      return data;
+    } catch (error) {
+      console.error("Login failed", error);
+      throw error;
+    }
+  };
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout, verifyOtp }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+
+
+
