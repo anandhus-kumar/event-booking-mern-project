d952d8de19ef4884efe95bdc8177679d5482e760
anandhus-kumar <anandhusk22@gmail.com>
2026-04-22 23:58:58 +0530

    style:navbar color change with scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
+  const heroRef = useRef(null);
+  const [isHeroVisible, setIsHeroVisible] = useState(true);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsHeroVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.3,
+      },
+    );
+
+    if (heroRef.current) {
+      observer.observe(heroRef.current);
+    }
+
+    return () => {
+      if (heroRef.current) {
+        observer.unobserve(heroRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className=" w-full min-h-screen">
-      <Navbar />
-      <Home />
+      <Navbar isHeroVisible={isHeroVisible} />
+      <Home heroRef={heroRef} />
     </div>
   );
 }
